a113198ad383d33481e00fb23510d6abcb7389c5
Daniel John <dj71286@gmail.com>
2023-10-06 21:09:42 +0100

    Added Redis and Mongodb clients

---
+++ utils/db.js
@@ -0,0 +1,35 @@
+const { MongoClient } = require('mongodb');
+import MongoClient from 'mongodb/lib/mongo_client';
+
+class DBClient {
+    constructor() {
+        this.host = 'localhost';
+        this.port = 27017;
+        this.database = 'jobs_vista';
+        this.url = `mongodb://${this.host}:${this.port}/${this.database}`;
+        this.client = new MongoClient(this.url, {useUnifiedTopology: true});
+        this.connect();
+    }
+    async connect() {
+        try {
+            await this.client.connect();
+            console.log('Connected to MongoDB');
+        } catch (error) {
+            console.error('Error connecting to MongoDB:', error.message);
+        }
+    }
+    
+    isAlive() {
+        return this.client.isConnected();
+    }
+    
+    async nbUsers() {
+        const usersCollection = this.client.db(this.database).collection('users');
+        const count = await usersCollection.countDocuments();
+        return count;
+    }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
