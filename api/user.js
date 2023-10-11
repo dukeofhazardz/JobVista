3cb70faf85a8d8a29e16cdab5a1515f16c8a2f27
Daniel John <dj71286@gmail.com>
2023-10-11 16:43:24 +0100

    Implemented session auth

--- api/user.js
+++ api/user.js
@@ -1,8 +1,26 @@
 import dbClient from "../utils/db";
+import redisClient from '../utils/redis';
+import { parse } from 'cookie';
+const { ObjectId } = require('mongodb');
 const User = {
     async nbUsers(req, res) {
         const userCount = await dbClient.nbUsers()
         return res.status(200).json({'Number of Users': userCount.toString()});
+    },
+
+    async getProfile(req, res) {
+        const cookies = parse(req.headers.cookie || '');
+        const token = cookies.sessionId;
+        if (!token) {
+            return res.redirect(301, '/login');
+        }
+        const key = `auth_${token}`;
+        const userId = await redisClient.get(key);
+        if (userId) {
+            const user = await dbClient.client.db(dbClient.database).collection('users').findOne({_id: ObjectId(userId)});
+            return res.render('profile', { user });
+        }
+        return res.redirect(301, '/');
     }
 };
 
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const upload = require('express-fileupload');
 
-const port = 5000
+const port = 7000
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
