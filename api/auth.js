9695af40062271740ae02bd8d631fdc2ef3e7686
Daniel John <dj71286@gmail.com>
2023-10-12 22:43:50 +0100

    Improved session auth cookies

--- api/auth.js
+++ api/auth.js
@@ -1,8 +1,5 @@
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
 import sha1 from 'sha1';
-import { v4 } from 'uuid';
-import { parse, serialize } from 'cookie';
 const { ObjectId } = require('mongodb');
 const path = require('path');
 const fs = require('fs');
@@ -11,26 +8,29 @@ let filePath = '';
 
 const Auth = {
     async getHome(req, res) {
-        let user = null;
-        const cookies = parse(req.headers.cookie || '');
-        const token = cookies.sessionId;
-        if (token) {
-            const key = `auth_${token}`;
-            const userId = await redisClient.get(key);
+        const userId = await req.redisClient.get('session:' + req.session.id);
+        console.log(userId)
+        console.log('session:' + req.session.id)
             if (userId) {
-                user = await dbClient.client.db(dbClient.database).collection('users').findOne({_id: ObjectId(userId)});
-            }
+                const user = await dbClient.client.db(dbClient.database).collection('users').findOne({_id: ObjectId(userId)});
+                return res.render('base', { user });
+            } else {
+            return res.render('base');
         }
-        const alt = 'dear';
-        return res.render('base', { user, alt });
     },
 
     async getSignup(req, res) {
-        return res.render('signup', {title: 'JobVista'});
+        return res.render('signup');
     },
 
     async getLogin(req, res) {
-        return res.render('login', {title: 'JobVista'});
+        const userId = await req.redisClient.get('session:' + req.session.id);
+            if (userId) {
+                const user = await dbClient.client.db(dbClient.database).collection('users').findOne({_id: ObjectId(userId)});
+                return res.render('login', { user });
+            } else {
+            return res.render('login');
+        }
     },
 
     async postSignup(req, res) {
@@ -83,6 +83,7 @@ const Auth = {
             resumePath: filePath,
         };
         await dbClient.client.db(dbClient.database).collection('users').insertOne(newUser);
+        res.cookie('email')
         return res.redirect(301, '/login');
 
     },
@@ -104,26 +105,21 @@ const Auth = {
         if (!user) {
             return res.status(400).json({error: `Password is incorrect`});
         }
-        const sessionId = v4();
-        const key = `auth_${sessionId}`;
-        const value = user._id.toString();
+        const userId = user._id.toString();
         const duration = 24 * 60 * 60; // 24 hours (in seconds)
-        res.setHeader('Set-Cookie', serialize('sessionId', sessionId, { httpOnly: true, maxAge: duration }));
-        await redisClient.set(key, value, duration);
-        res.set('Cookie', sessionId);
+        await req.redisClient.set('session:' + req.session.id, userId, duration);
         return res.redirect(301, '/');
     },
 
-    async logout(req, res) {
-        const cookies = parse(req.headers.cookie || '');
-        const token = cookies.sessionId;
-        
-        if (token) {
-            const key = `auth_${token}`;
-            await redisClient.del(key);
-            res.setHeader('Set-Cookie', serialize('sessionId', '', { httpOnly: true, expires: new Date(0) }));
-        }
-        return res.redirect(301, '/login');
+    async postLogout(req, res) {
+        await req.redisClient.del('session:' + req.session.id)
+        req.session.destroy((err) => {
+            if (err) {
+                console.log('Error destorying session:', err);
+            } else {
+                return res.redirect(301, '/login');
+            }
+        });
     },
 };
 
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,37 @@
 import Auth from '../api/auth'
 import User from '../api/user';
 import Jobs from '../api/jobs';
+import redisClient from '../utils/redis';
 const express = require('express');
+const session = require('express-session');
 
 const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
+router.use(session({
+    secret: 'jobvistasecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false, // set to true after https
+        sameSite: 'strict',
+    }
+}));
+router.use((req, res, next) => {
+    req.redisClient = redisClient;
+    next();
+});
 
 router.get('/', Auth.getHome);
-router.get('/logout', Auth.logout);
 router.get('/usercount', User.nbUsers);
 router.get('/signup', Auth.getSignup);
 router.get('/login', Auth.getLogin);
 router.post('/signup', Auth.postSignup);
 router.post('/login', Auth.postLogin);
+router.post('/logout', Auth.postLogout);
 router.get('/profile', User.getProfile);
+router.get('/settings', User.getSettings);
+router.post('/settings', User.postSettings);
+router.get('/download', User.getResume);
 
 export default router;
